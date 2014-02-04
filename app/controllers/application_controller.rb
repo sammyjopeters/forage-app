89eb305d878d6db62a967d2d32f2f5c87b1b279d
sammyjopeters <peters.sammyjo@gmail.com>
2014-02-05 10:58:46 +1100

    Fixed user autherisation, style tweaking, fix homepage style

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!, except: [:index]
 
-    def authenticate_user
-  	if session[:user_id]
-  		@current_user = User.find(session[:user_id])
-  	else
-  		redirect_to new_session_path, :notice => 'please login.'
-  	end
+
+    # #takes you back to the page you were on once you've signed in.
+    # def after_sign_in_path_for(resource)
+    # request.env['omniauth.origin'] || stored_location_for(resource) || root_path
+    # end
+
+    # def after_sign_in_path_for(resource)
+    # user_listings_path(current_user.id)
+    # end
 
 
-  end
 end
