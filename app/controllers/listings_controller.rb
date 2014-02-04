8d023137c1ed546bf0ceb60dda242068573d15f5
sammyjopeters <peters.sammyjo@gmail.com>
2014-02-04 21:06:18 +1100

    Category button styles, text colour fixed

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,5 +1,5 @@
 class ListingsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_listing, only: [:show, :edit, :update, :destroy]
   before_action :set_user, only: [:index, :new, :create]
   
