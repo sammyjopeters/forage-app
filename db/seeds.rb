fd9c778d1ba784e0ff063f0882cf4d16a6db5158
sammyjopeters <peters.sammyjo@gmail.com>
2014-02-05 12:04:24 +1100

    updated seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,10 @@
 
 User.create(
 	name: 'Sam P',
-	email_address: 'sretepmas@gmail.com',
+	email: 'sretepmas@gmail.com',
 	username: 'Sammyjo',
-	password: 'generalassembly',
-	home_location: 'Lane Cove',
+	password: 'fredfred',
+	home_location: 'Sydney',
 	home_state: 'NSW',
 	merchant_type: 'Home Grower',
 	avatar: 'http://lorempixel.com/150/150/',
@@ -19,19 +19,16 @@ User.create(
 
 	)
 
-Listing.create(
-	goods_type: 'Vegetables',
-	amount: 6,
-	description: 'My carrot crop has done really well this year, and I\'ve got a 6 spare! They\'re an heirloom variety and are grown in a pesticide free environment! Pick up from my office.',
-	location: 'Ultimo',
-	location_state: 'NSW',
-	transaction_type: 'Grab',
-	cost: nil,
-	expiry: "#{1.week.from_now}",
-	user_id: 1,
-	pictures: 'http://zoebakes.com/wp-content/uploads/2008/04/carrot-cake01.jpg',
-	sold: false,
-	sold_to: nil
+User.create(
+	name: 'Fred Z',
+	email: 'fred@mailinator.com',
+	username: 'Fredd13',
+	password: 'fredfred',
+	home_location: 'Ultimo',
+	home_state: 'NSW',
+	merchant_type: 'Corporate Agriculture Spy',
+	avatar: 'http://lorempixel.com/150/150/',
+	purchases: nil
 
 	)
 
@@ -42,3 +39,44 @@ Category.create(
 Category.create(
 	name: 'Fruit'
 	)
+
+Category.create(
+	name: 'Dairy'
+	)
+
+
+
+Listing.create(
+	goods_type: 'Carrots',
+	amount: 6,
+	description: 'My carrot crop has done really well this year, and I\'ve got a 6 spare! They\'re an heirloom variety and are grown in a pesticide free environment! Pick up from my office.',
+	location: 'Panania',
+	location_state: 'NSW',
+	transaction_type: 'Grab',
+	cost: nil,
+	expiry: "#{1.week.from_now}",
+	user_id: 1,
+	pictures: 'http://zoebakes.com/wp-content/uploads/2008/04/carrot-cake01.jpg',
+	sold: false,
+	sold_to: nil,
+	category_id: 1
+
+	)
+
+Listing.create(
+	goods_type: 'Cheese wheels',
+	amount: 486,
+	description: 'I have 3 cheese factories I run in my spare time, and I\'m selling the excess!',
+	location: 'Ultimo',
+	location_state: 'NSW',
+	transaction_type: 'Buy',
+	cost: 80,
+	expiry: "#{1.week.from_now}",
+	user_id: 2,
+	pictures: 'http://www.thelarder489.com.au/thumbnaillarge/cheese45.jpg',
+	sold: false,
+	sold_to: nil,
+	category_id: 3
+
+	)
+
